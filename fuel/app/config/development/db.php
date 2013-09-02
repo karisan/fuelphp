cd6089278d92134d8e06bd2965cfd792e27fffa9
karisan <karis083@hotmail.com>
2013-09-02 14:20:42 +0800

    拆解的資料，以Redis依序存、取

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=test',
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=test',
 			'username'   => 'root',
 			'password'   => 'mysql1234',
 		),
